81e0c10e8d53a11f0f90f83def6a7ded6124540a
Paweł Ropa <pawel.ropa@gmail.com>
2017-04-01 18:08:18 +0200

    Created a spec file for carthridge.js

---
+++ app/spec/cartridgeSpec.js
@@ -0,0 +1,16 @@
+describe("A suite is just a function", function() {
+  var a;
+
+  it("and so is a spec", function() {
+    a = true;
+
+    expect(a).toBe(true);
+  });
+
+  it("another test", function() {
+    a = false;
+
+    expect(a).toBe(false);
+
+  })
+});
