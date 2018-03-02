af709c293d7bac2e2639cf951c930e2be632698d
Paweł Ropa <pawel.ropa@gmail.com>
2018-03-02 08:09:47 +0100

    Create opcodes table and functions skeletons

--- app/js/cpu.js
+++ app/js/cpu.js
@@ -19,7 +19,7 @@ var insMode = [
 	10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
 ];
 
-var insSize = [ 
+var insSize = [
 	1, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
 	2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
 	3, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
@@ -38,7 +38,7 @@ var insSize = [
 	2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
 ];
 
-var insCycle = [ 
+var insCycle = [
 	7, 6, 2, 8, 3, 3, 5, 5, 3, 2, 2, 2, 4, 4, 6, 6,
 	2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
 	6, 6, 2, 8, 3, 3, 5, 5, 4, 2, 2, 2, 4, 4, 6, 6,
@@ -76,111 +76,172 @@ var insPageCycle = [
 	1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0,
 ];
 
-var opcodes = [
-	"BRK", "ORA", "KIL", "SLO", "NOP", "ORA", "ASL", "SLO",
-	"PHP", "ORA", "ASL", "ANC", "NOP", "ORA", "ASL", "SLO",
-	"BPL", "ORA", "KIL", "SLO", "NOP", "ORA", "ASL", "SLO",
-	"CLC", "ORA", "NOP", "SLO", "NOP", "ORA", "ASL", "SLO",
-	"JSR", "AND", "KIL", "RLA", "BIT", "AND", "ROL", "RLA",
-	"PLP", "AND", "ROL", "ANC", "BIT", "AND", "ROL", "RLA",
-	"BMI", "AND", "KIL", "RLA", "NOP", "AND", "ROL", "RLA",
-	"SEC", "AND", "NOP", "RLA", "NOP", "AND", "ROL", "RLA",
-	"RTI", "EOR", "KIL", "SRE", "NOP", "EOR", "LSR", "SRE",
-	"PHA", "EOR", "LSR", "ALR", "JMP", "EOR", "LSR", "SRE",
-	"BVC", "EOR", "KIL", "SRE", "NOP", "EOR", "LSR", "SRE",
-	"CLI", "EOR", "NOP", "SRE", "NOP", "EOR", "LSR", "SRE",
-	"RTS", "ADC", "KIL", "RRA", "NOP", "ADC", "ROR", "RRA",
-	"PLA", "ADC", "ROR", "ARR", "JMP", "ADC", "ROR", "RRA",
-	"BVS", "ADC", "KIL", "RRA", "NOP", "ADC", "ROR", "RRA",
-	"SEI", "ADC", "NOP", "RRA", "NOP", "ADC", "ROR", "RRA",
-	"NOP", "STA", "NOP", "SAX", "STY", "STA", "STX", "SAX",
-	"DEY", "NOP", "TXA", "XAA", "STY", "STA", "STX", "SAX",
-	"BCC", "STA", "KIL", "AHX", "STY", "STA", "STX", "SAX",
-	"TYA", "STA", "TXS", "TAS", "SHY", "STA", "SHX", "AHX",
-	"LDY", "LDA", "LDX", "LAX", "LDY", "LDA", "LDX", "LAX",
-	"TAY", "LDA", "TAX", "LAX", "LDY", "LDA", "LDX", "LAX",
-	"BCS", "LDA", "KIL", "LAX", "LDY", "LDA", "LDX", "LAX",
-	"CLV", "LDA", "TSX", "LAS", "LDY", "LDA", "LDX", "LAX",
-	"CPY", "CMP", "NOP", "DCP", "CPY", "CMP", "DEC", "DCP",
-	"INY", "CMP", "DEX", "AXS", "CPY", "CMP", "DEC", "DCP",
-	"BNE", "CMP", "KIL", "DCP", "NOP", "CMP", "DEC", "DCP",
-	"CLD", "CMP", "NOP", "DCP", "NOP", "CMP", "DEC", "DCP",
-	"CPX", "SBC", "NOP", "ISC", "CPX", "SBC", "INC", "ISC",
-	"INX", "SBC", "NOP", "SBC", "CPX", "SBC", "INC", "ISC",
-	"BEQ", "SBC", "KIL", "ISC", "NOP", "SBC", "INC", "ISC",
-	"SED", "SBC", "NOP", "ISC", "NOP", "SBC", "INC", "ISC",
-]
+
 (function CPU() {
-  var acc = 0x00, // Accumulator
-      x = 0x00, // X register
-      y = 0x00, // Y register
-      sp = 0x00, // Stack pointer
+	var acc = 0x00, // Accumulator
+		x = 0x00, // X register
+		y = 0x00, // Y register
+		sp = 0x00, // Stack pointer
 
-      // Flags
-      carry = 0,
-      zero = 0,
-      interrupt = 0,
-      decimalMode = 0,
-      brk = 0, //Break,
-      unused = 0,
-      overflow = 0,
-      sign = 0,
+		// Flags
+		carry = 0,
+		zero = 0,
+		interrupt = 0,
+		decimalMode = 0,
+		brk = 0, //Break,
+		unused = 0,
+		overflow = 0,
+		sign = 0,
 
-      pc =  0x0000, // Program counter - 16 bit
-      memory = Uint8Array(65535); // 0xFFFF
+		pc = 0x0000, // Program counter - 16 bit
+		memory = Uint8Array(65535); // 0xFFFF
 
-  var mem_read = function(addr) {
-    if (addr < 0x800) {
-      return memory[addr];
-    } else {
-      return mem_read_other(addr);
-    }
-  };
+	var mem_read = function (addr) {
+		if (addr < 0x800) {
+			return memory[addr];
+		} else {
+			return mem_read_other(addr);
+		}
+	};
 
-  var mem_read_other = function() {
-    if (addr < 0x2000) {
-      return ram[addr & 0x7FF];
-    } else if (addr <= 0x3FFF) {
-		  return ppu_read(addr);
-    } else if (addr >= 0x4000 && addr <= 0x4017) {
-		  return apu_read(addr);
-    } else {
-      return mem_read_fp[addr >> 12](addr);
-    }
-  };
+	var mem_read_other = function () {
+		if (addr < 0x2000) {
+			return ram[addr & 0x7FF];
+		} else if (addr <= 0x3FFF) {
+			return ppu_read(addr);
+		} else if (addr >= 0x4000 && addr <= 0x4017) {
+			return apu_read(addr);
+		} else {
+			return mem_read_fp[addr >> 12](addr);
+		}
+	};
+	var adc = function () { };
+	var ahx = function () { };
+	var alr = function () { };
+	var anc = function () { };
+	var and = function () { };
+	var arr = function () { };
+	var asl = function () { };
+	var axs = function () { };
+	var bcc = function () { };
+	var bcs = function () { };
+	var beq = function () { };
+	var bit = function () { };
+	var bmi = function () { };
+	var bne = function () { };
+	var bpl = function () { };
+	var brk = function () { };
+	var bvc = function () { };
+	var bvs = function () { };
+	var clc = function () { };
+	var cld = function () { };
+	var cli = function () { };
+	var clv = function () { };
+	var cmp = function () { };
+	var cpx = function () { };
+	var cpy = function () { };
+	var dcp = function () { };
+	var dec = function () { };
+	var dex = function () { };
+	var dey = function () { };
+	var eor = function () { };
+	var inc = function () { };
+	var inx = function () { };
+	var iny = function () { };
+	var isc = function () { };
+	var jmp = function () { };
+	var jsr = function () { };
+	var kil = function () { };
+	var las = function () { };
+	var lax = function () { };
+	var lda = function () { };
+	var ldx = function () { };
+	var ldy = function () { };
+	var lsr = function () { };
+	var nop = function () { };
+	var ora = function () { };
+	var pha = function () { };
+	var php = function () { };
+	var pla = function () { };
+	var plp = function () { };
+	var rla = function () { };
+	var rol = function () { };
+	var ror = function () { };
+	var rra = function () { };
+	var rti = function () { };
+	var rts = function () { };
+	var sax = function () { };
+	var sbc = function () { };
+	var sec = function () { };
+	var sed = function () { };
+	var sei = function () { };
+	var shx = function () { };
+	var shy = function () { };
+	var slo = function () { };
+	var sre = function () { };
+	var sta = function () { };
+	var stx = function () { };
+	var sty = function () { };
+	var tas = function () { };
+	var tax = function () { };
+	var tay = function () { };
+	var tsx = function () { };
+	var txa = function () { };
+	var txs = function () { };
+	var tya = function () { };
+	var xaa = function () { };
+
+	var opcodes = [
+		brk, ora, kil, slo, nop, ora, asl, slo, php, ora, asl, anc, nop, ora, asl, slo,
+		bpl, ora, kil, slo, nop, ora, asl, slo, clc, ora, nop, slo, nop, ora, asl, slo,
+		jsr, and, kil, rla, bit, and, rol, rla, plp, and, rol, anc, bit, and, rol, rla,
+		bmi, and, kil, rla, nop, and, rol, rla, sec, and, nop, rla, nop, and, rol, rla,
+		rti, eor, kil, sre, nop, eor, lsr, sre, pha, eor, lsr, alr, jmp, eor, lsr, sre,
+		bvc, eor, kil, sre, nop, eor, lsr, sre, cli, eor, nop, sre, nop, eor, lsr, sre,
+		rts, adc, kil, rra, nop, adc, ror, rra, pla, adc, ror, arr, jmp, adc, ror, rra,
+		bvs, adc, kil, rra, nop, adc, ror, rra, sei, adc, nop, rra, nop, adc, ror, rra,
+		nop, sta, nop, sax, sty, sta, stx, sax, dey, nop, txa, xaa, sty, sta, stx, sax,
+		bcc, sta, kil, ahx, sty, sta, stx, sax, tya, sta, txs, tas, shy, sta, shx, ahx,
+		ldy, lda, ldx, lax, ldy, lda, ldx, lax, tay, lda, tax, lax, ldy, lda, ldx, lax,
+		bcs, lda, kil, lax, ldy, lda, ldx, lax, clv, lda, tsx, las, ldy, lda, ldx, lax,
+		cpy, cmp, nop, dcp, cpy, cmp, dec, dcp, iny, cmp, dex, axs, cpy, cmp, dec, dcp,
+		bne, cmp, kil, dcp, nop, cmp, dec, dcp, cld, cmp, nop, dcp, nop, cmp, dec, dcp,
+		cpx, sbc, nop, isc, cpx, sbc, inc, isc, inx, sbc, nop, sbc, cpx, sbc, inc, isc,
+		beq, sbc, kil, isc, nop, sbc, inc, isc, sed, sbc, nop, isc, nop, sbc, inc, isc
+	]
 })();
 
 class CPU {
-  // constructor(acc, x, y, sp, sf, pc, c, z, i, d, b, u, o, s) {
-  //   this.acc = acc;
-  //   this.x = x;
-  //   this.y = y;
-  //   this.sp = sp;
-  //   this.sf = sf;
-  //   this.pc = pc;
-  //   this.c = c; //Carry flag
-  //   this.z = z; //Zero flag
-  //   this.i = i; //Interrupt flag
-  //   this.d = d; //Decimal flag
-  //   this.b = b; //Break flag
-  //   this.u = u; //Unused flag
-  //   this.o = o; //Overflow flag
-  //   this.s = s; //Sign flag
-  // }
+	// constructor(acc, x, y, sp, sf, pc, c, z, i, d, b, u, o, s) {
+	//   this.acc = acc;
+	//   this.x = x;
+	//   this.y = y;
+	//   this.sp = sp;
+	//   this.sf = sf;
+	//   this.pc = pc;
+	//   this.c = c; //Carry flag
+	//   this.z = z; //Zero flag
+	//   this.i = i; //Interrupt flag
+	//   this.d = d; //Decimal flag
+	//   this.b = b; //Break flag
+	//   this.u = u; //Unused flag
+	//   this.o = o; //Overflow flag
+	//   this.s = s; //Sign flag
+	// }
 
-  getBit(value, position) {
-    return (value >> position) & 1;
-  }
+	getBit(value, position) {
+		return (value >> position) & 1;
+	}
 
-  setBit(value, position) {
-    return value | (1 << position)
-  }
+	setBit(value, position) {
+		return value | (1 << position)
+	}
 
-  clearBit(value, position) {
-    return value & ~(1 << position)
-  }
+	clearBit(value, position) {
+		return value & ~(1 << position)
+	}
 }
 
 module.exports = {
-  CPU: CPU
+	CPU: CPU
 };
